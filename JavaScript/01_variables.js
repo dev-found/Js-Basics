05874d4b20012b9d9cdf2a79ea8d5a3eb70a169a
dev-found <leviosa674@gmail.com>
2024-09-10 14:29:39 +0000

    01_varaibles added

---
+++ JavaScript/01_variables.js
@@ -0,0 +1,15 @@
+//VARIABLES AND CONSTANTS
+
+//creating constants
+const accountId = 142
+let accountEmail = "142@yahoo.com"
+var accountPassword = "321"
+/*
+accountId = 143 // not allowed
+note: const cannot be changed or modified once created
+*/
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword])
+
