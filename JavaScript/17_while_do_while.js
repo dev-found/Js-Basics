cedffe3a29e3b50633066a6557fea99848af2aa3
dev-found <leviosa674@gmail.com>
2024-09-16 12:17:06 +0000

    do while and while loops

---
+++ JavaScript/17_while_do_while.js
@@ -0,0 +1,32 @@
+// ++++++++ while loop +++++++++ //
+
+let index = 0
+while (index <= 10) {
+    // console.log(`value of idnex is ${index}`);
+    index = index + 2
+}
+
+let supes = ['homelander', 'starlight', 'ATrain', 'Butcher']
+// console.log(supes.length);
+
+let arr = 0
+while (arr < supes.length) {
+    // console.log(supes[arr]);
+    arr = arr + 1
+}
+
+// +++++++++ do while ++++++++++ //
+
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++
+    
+} while (score <= 5);
+
+let scores = 11
+do {
+    console.log(`Score is ${scores}`);
+    score++
+    
+} while (scores <= 10);
